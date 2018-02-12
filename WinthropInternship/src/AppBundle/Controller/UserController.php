005427ebf49be2e2051a818c91f18209ce4f1ce4
Cathy Guilbaud <guilbaudc2@mailbox.winthrop.edu>
2018-02-12 00:06:18 +0000

    Completed styling of the home and login pages. Routing for the pages needs to be adjusted

---
+++ WinthropInternship/src/AppBundle/Controller/UserController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * User controller.
+ *
+ * @Route("user")
+ */
+class UserController extends Controller
+{
+    /**
+     * Lists all user entities.
+     *
+     * @Route("/", name="user_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $users = $em->getRepository('AppBundle:User')->findAll();
+
+        return $this->render('user/index.html.twig', array(
+            'users' => $users,
+        ));
+    }
+
+    /**
+     * Finds and displays a user entity.
+     *
+     * @Route("/{id}", name="user_show")
+     * @Method("GET")
+     */
+    public function showAction(User $user)
+    {
+
+        return $this->render('user/show.html.twig', array(
+            'user' => $user,
+        ));
+    }
+}
