ef7aeb235990d5cc427952f774b283c60bac7b9a
Murphys95 <35769856+Murphys95@users.noreply.github.com>
2018-02-17 16:44:19 -0500

    Second Commit

---
+++ StudentForm1_Help.php
@@ -0,0 +1,100 @@
+<html>
+<body>
+
+
+<div style="background: #024366 ">
+    <font color="white">WILE</font>
+</div>
+<div style="text-align: center; background: coral">
+    <font color="white">
+        The language in this form does not create an employment contract, either expressed<br>
+        or implied, or otherwise alter the at-will employment relationship between any<br>
+        employee and Winthrop University. Winthrop University reserves the right to revise<br>
+        the contents of this form in whole or in part, as necessary.<br>
+    </font>
+</div>
+
+<h1 style="text-align: center">
+    Internship Learning Agreement Form
+</h1>
+<br>
+<form action="StudentForm1_Help.php">
+    <input type="submit" value="Help">
+</form>
+<h1 style="text-align: center">
+    Your Information
+</h1>
+
+
+<form>
+
+    Student ID #(W Number)<br>
+    <input type="text" name="wNumber"><br>
+    Helper text<br>
+
+    Email Address<br>
+    <input type="text" name="Email"><br>
+    Helper text<br>
+
+    Class/Graduation Year<br>
+    <select name="GradMonth">
+        <option value="May">May</option>
+        <option value="August">August</option>
+        <option value="December">December</option>
+    </select>
+    <input type="text" name="GradYear"><br>
+    Helper text<br>
+
+    <input type="text" name="Name" value="Legal Name"><br>
+    Helper text<br>
+
+    <input type="text" name="Phone" value="Phone Number"><br>
+    Helper text<br>
+
+    <input type="text" name="EnrolledYear" value="Semester/Year Enrolled"><br>
+    Helper text<br>
+
+    Class Enrolled in<br>
+    <select name="Class">
+        <option value="CSCI491">CSCI 491</option>
+        <option value="....">....</option>
+        <option value="....">....</option>
+    </select><br>
+    Helper text<br>
+
+    <input type="text" name="Major" value="Major"><br>
+    Helper text<br>
+
+    <input type="text" name="Supervisor" value="Site Supervisor"><br>
+    Helper text<br>
+
+    Site Supervisor Email Address<br>
+    <input type="text" name="supervisorEmail"><br>
+    Helper text<br>
+
+    <select name="FacultyLiaison" value="Faculty Liaison">
+        <option value="CSCI491">CSCI 491</option>
+        <option value="....">....</option>
+        <option value="....">....</option>
+    </select><br>
+    Helper text<br>
+
+    <input type="text" name="Minor" value="Minor/Concentration"><br>
+    Helper text<br><br>
+
+    <input type="submit" value="SAVE">
+
+    <input type="submit" value="SUBMIT">
+
+</form>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sean9_000
+ * Date: 2/10/2018
+ * Time: 9:37 PM
+ */
+?>
+</body>
+</html>
