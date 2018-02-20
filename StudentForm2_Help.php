58cb56268ff448f57dd747d7255c3fe3a1ae9184
Murphys95 <35769856+Murphys95@users.noreply.github.com>
2018-02-20 17:03:18 -0500

    Third Commit

---
+++ StudentForm2_Help.php
@@ -0,0 +1,69 @@
+<html>
+<body>
+
+
+<div style="background: #024366 ">
+    <font color="white">WILE</font>
+</div>
+<div style="text-align: center; background: coral">
+    <font color="white">
+        The language in this form does not create an employment contract, either expressed<br>
+        or implied, or otherwise alter the at-will employment relationship between any<br>
+        employee and Winthrop University. Winthrop University reserves the right to revise<br>
+        the contents of this form in whole or in part, as necessary.<br>
+    </font>
+</div>
+
+<h1 style="text-align: center">
+    Personal Learning Goals
+</h1>
+<br>
+
+<!-***************************** ->
+<!-STATUS BAR NEEDS TO BE PLACED ->
+<!-***************************** ->
+
+<form action="StudentForm2_Help.php">
+    <input type="submit" value="Help">
+</form>
+<h1 style="text-align: center">
+    Your Information
+</h1>
+
+<!-Form not assigned yet->
+<form>
+
+    Explain how this internship will add to your educational experience at Winthrop<br>
+    University <br>
+    <input style="height:10%; width:50%" type="text" name="educationExperience"><br>
+    Helper text<br><br>
+
+    What professional and personal goals do you hope to achieve while at this internship? (Be specific)<br>
+    <input style="height:10%; width:50%" type="text" name="Goals"><br>
+    Helper text<br><br>
+
+    By signing and dating below as my electronic signature, I hearby <br>
+    certify that I have read the agreement and will fulfill the duties <br>
+    and responsibilities outlined for the internship and the academic <br>
+    requirements for completeing the internship course for credit. <br>
+
+
+    <input type="text" name="legalName" value="Legal Name">
+    <input type="text" name="dateSigned" value="Today's Date"><br>
+
+    <input type="submit" value="SAVE">
+
+    <input type="submit" value="SUBMIT">
+
+</form>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sean9_000
+ * Date: 2/10/2018
+ * Time: 9:37 PM
+ */
+?>
+</body>
+</html>
